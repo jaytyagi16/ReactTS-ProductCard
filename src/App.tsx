824ddab2392f01cb07ce72668b0cb32ed8d50cfd
Jay Tyagi <jaytyagi16@gmail.com>
2025-02-19 17:05:35 +0530

    Props added in ProductCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import ProductCard from './components/ProductCard'
+import courseImage from "../src/assets/course_1662795698.jpg"
 
 const App = () => {
   return (
     <div>
       <div className='m-10'>
-        <ProductCard/>
+        <ProductCard image={courseImage} likes={76} views={12.3} author={"Mako Tserteli"}/>
       </div>
 
     </div>
